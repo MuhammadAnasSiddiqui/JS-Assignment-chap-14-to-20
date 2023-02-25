6d3801407e2ac23bdfe48fa26ef1623215aa3b8b
Muhammad Anas siddiqui <muhammadanassiddiqui0@gmail.com>
2023-02-25 22:18:24 +0500

    Assignment complete

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ var std1Percentage = stdMarks[0] * 100 / totalMarks;
 var std2Percentage = stdMarks[1] * 100 / totalMarks;
 var std3Percentage = stdMarks[2] * 100 / totalMarks;
 
-document.write( + "<br>" + "<br>" + "<br>")
+document.write(+ "<br>" + "<br>" + "<br>")
 
 document.write("Score of " + stdArr[0] + " " + "is " + stdMarks[0] + ". Percentage: " + std1Percentage + "<br>")
 document.write("Score of " + stdArr[1] + " " + "is " + stdMarks[1] + ". Percentage: " + std2Percentage + "<br>")
@@ -50,7 +50,7 @@ document.write("Score of " + stdArr[2] + " " + "is " + stdMarks[2] + ". Percenta
 
 // Question#9
 
-var colorsArr = ["Red", "Black" ,"Pink"];
+var colorsArr = ["Red", "Black", "Pink"];
 document.write(colorsArr + "<br>");
 
 // // part(a)
@@ -90,51 +90,175 @@ document.write(colorsArr + "<br>" + "<br>" + "<br>");
 // document.write(colorsArr + "<br>" + "<br>" + "<br>");
 
 // question#11
-var cities = ["Karachi","LAHORE","Islamabad","Quetta", "Peshawar"];
-var selectedCities = cities.slice(2,4);
-document.write("Cities list:" + "<br>" + cities + "<br>"  + "<br>" + "Selected cities list:" + "<br>" + selectedCities);
+var cities = ["Karachi", "LAHORE", "Islamabad", "Quetta", "Peshawar"];
+var selectedCities = cities.slice(2, 4);
+document.write("Cities list:" + "<br>" + cities + "<br>" + "<br>" + "Selected cities list:" + "<br>" + selectedCities);
 
 // question#12
-var arr = ["This " , "is " , "my  " , "cat"];
+var arr = ["This ", "is ", "my  ", "cat"];
 document.write("<h3>" + "Array:" + "<br>" + arr + "</h3>");
 var stringArr = arr.join("");
 document.write("<h3>" + "string:" + "<br>" + stringArr + "</h3>");
 
 // question#13
 
-var pcDevices = [];
-pcDevices.push("keyboard");
-pcDevices.push("mouse");
-pcDevices.push("printer");
-pcDevices.push("monitor");
+var devises = ["keyboard", "mouse", "printer", "monitor"];
 
-document.write("<h1> Devices:" + "<br/>" + pcDevices + "</h1>");
+document.write("<h1> Devices: " + "<br/>" + devises + "</h1>");
 
-document.write("<h1> Out:" + "<br/>" + pcDevices.shift() + "</h1>");
-document.write("<h1> Out:" + "<br/>" + pcDevices.shift() + "</h1>");
-document.write("<h1> Out:" + "<br/>" + pcDevices.shift() + "</h1>");
-document.write("<h1> Out:" + "<br/>" + pcDevices.shift() + "</h1>");
+for (var i = 0; i < devises.length; i++) {
+    document.write("<h1> Out:" + "<br/>" + devises[i] + "</h1>");
+}
+document.write("<br/>" + "<br/>")
 
 // question#14
+var devises = ["keyboard", "mouse", "printer", "monitor"];
 
-var pcDevices = [];
+document.write("<h1> Devices: " + "<br/>" + devises + "</h1>");
 
-pcDevices.push("keyboard");
-pcDevices.push("mouse");
-pcDevices.push("printer");
-pcDevices.push("monitor");
-
-document.write("<h1> Devices:" + "<br/>" + pcDevices + "</h1>");
-
-document.write("<h1> Out:" + "<br/>" + pcDevices.pop() + "</h1>");
-document.write("<h1> Out:" + "<br/>" + pcDevices.pop() + "</h1>");
-document.write("<h1> Out:" + "<br/>" + pcDevices.pop() + "</h1>");
-document.write("<h1> Out:" + "<br/>" + pcDevices.pop() + "</h1>");
+for (var i = devises.length - 1; i >= 0; i--) {
+    document.write("<h1> Out:" + "<br/>" + devises[i] + "</h1>");
+}
 
 // question#15
-var mobDevices = ["Apple", "Samsung", "Motorola", "Nokia","Sony","Haier"];
+var mobDevices = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
 document.write("<select>")
-for(var i = 0 ; i < mobDevices.length; i++){
-document.write("<option> " + mobDevices[i] + "</option>")
+for (var i = 0; i < mobDevices.length; i++) {
+    document.write("<option> " + mobDevices[i] + "</option>")
 }
-document.write("</select>")
+document.write("</select>");
+document.write("<br>" + "<br>" + "<br>");
+
+// chap# 14 to 16
+
+// question#1
+var mulArr = [[]];
+console.log(mulArr);
+
+// question#2
+var mulArr = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
+
+for (var i = 0; i < mulArr.length; i++) {
+    document.write(mulArr[i] + "<br>")
+}
+
+document.write("<br>" + "<br>" + "<br>");
+
+// question#3
+for (var i = 1; i <= 10; i++) {
+    document.write(i + "<br>")
+}
+
+document.write("<br>" + "<br>" + "<br>");
+
+// question#4
+// var tableNum = +prompt("Enter a number to show its multiplicationntable");
+// var tableLenght = +prompt("Enter lenght multiplication table");
+
+// for(var i = 1 ; i <= tableLenght ; i++){
+//     document.write(tableNum + " x " + i + " = " + tableNum * i + "<br>" + "<br>" + "<br>")
+// }
+
+// question#5
+
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+
+for (var i = 0; i < fruits.length; i++) {
+    document.write(fruits[i] + "<br>")
+}
+
+document.write("<br>");
+
+for (var i = 0; i < fruits.length; i++) {
+    document.write("Element at index " + i + " is " + fruits[i], "<br>")
+}
+
+document.write("<br>" + "<br>" + "<br>");
+
+// question#6
+
+// (a)
+for (var i = 1; i <= 15; i++) {
+    document.write(i + "<br>")
+}
+
+document.write("<br>" + "<br>" + "<br>");
+
+// (b)
+for (var i = 10; i > 0; i--) {
+    document.write(i + "<br>")
+}
+
+document.write("<br>" + "<br>" + "<br>");
+
+// (c)
+for (var i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+        document.write(i + "<br>")
+    }
+}
+
+document.write("<br>" + "<br>" + "<br>");
+
+// (d)
+for (var i = 1; i <= 19; i++) {
+    if (i % 2 !== 0) {
+        document.write(i + "<br>")
+    }
+}
+
+document.write("<br>" + "<br>" + "<br>");
+
+// (e)
+for (var i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        document.write(i + "k" + "<br>")
+    }
+}
+
+document.write("<br>" + "<br>" + "<br>");
+
+// question#7
+// var bakers = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var user = prompt("Welcome to ABC Bakery .What do you want to order sir/ma'am?");
+
+// var isMatch = false;
+// for (var i = 0; i < bakers.length; i++) {
+//     if (bakers[i] === user) {
+//         isMatch = true;
+//         document.write("We are sorry. " + user + " is available at index " + i + " in our bakery" + "<br>")
+//         break;
+//     }
+// }
+// if (isMatch === false) {
+//     document.write(user + " is not available in our bakery" + "<br>")
+// }
+
+// question#8
+var largestNum = [24, 53, 78, 91, 12];
+var tempNum = 0;
+
+for (var i = 0; i < largestNum.length; i++) {
+    if (tempNum < largestNum[i]) {
+        tempNum = largestNum[i]
+    }
+}
+document.write("Array items: " + largestNum + "<br>");
+document.write("The largest number is " + tempNum + "<br>" + "<br>" + "<br>");
+
+// question#9
+var smallestNum = [24, 53, 78, 91, 12];
+var tempNum = smallestNum[0];
+
+for (var i = 0; i < smallestNum.length; i++) {
+    if (tempNum > smallestNum[i]) {
+        tempNum = smallestNum[i]
+    }
+}
+document.write("Array items: " + smallestNum + "<br>");
+document.write("The smallest number is " + tempNum + "<br>" + "<br>" + "<br>");
+
+// question#10
+for (var i = 1; i <= 20; i++) {
+    document.write(5 * i + " ,")
+}
